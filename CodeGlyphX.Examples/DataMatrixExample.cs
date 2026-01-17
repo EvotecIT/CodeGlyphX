c750e72635ad888357c28a820871b3bc250ec812
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2026-01-17 10:16:04 +0100

    Add PDF/EPS example outputs and vector tests

--- CodeGlyphX.Examples/DataMatrixExample.cs
+++ CodeGlyphX.Examples/DataMatrixExample.cs
@@ -7,8 +7,12 @@ internal static class DataMatrixExample {
     public static void Run(string outputDir) {
         var pngPath = Path.Combine(outputDir, "datamatrix.png");
         var svgPath = Path.Combine(outputDir, "datamatrix.svg");
+        var pdfPath = Path.Combine(outputDir, "datamatrix.pdf");
+        var epsPath = Path.Combine(outputDir, "datamatrix.eps");
 
         DataMatrixCode.Save("DataMatrix-HELLO", pngPath);
         DataMatrixCode.Save("DataMatrix-HELLO", svgPath);
+        DataMatrixCode.Save("DataMatrix-HELLO", pdfPath);
+        DataMatrixCode.Save("DataMatrix-HELLO", epsPath);
     }
 }
--- CodeGlyphX.Examples/Pdf417Example.cs
+++ CodeGlyphX.Examples/Pdf417Example.cs
@@ -7,8 +7,12 @@ internal static class Pdf417Example {
     public static void Run(string outputDir) {
         var pngPath = Path.Combine(outputDir, "pdf417.png");
         var svgPath = Path.Combine(outputDir, "pdf417.svg");
+        var pdfPath = Path.Combine(outputDir, "pdf417.pdf");
+        var epsPath = Path.Combine(outputDir, "pdf417.eps");
 
         Pdf417Code.Save("PDF417-HELLO", pngPath);
         Pdf417Code.Save("PDF417-HELLO", svgPath);
+        Pdf417Code.Save("PDF417-HELLO", pdfPath);
+        Pdf417Code.Save("PDF417-HELLO", epsPath);
     }
 }
