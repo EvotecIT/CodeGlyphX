c18df7719020444c84c1331b4601f8d4b3d19d42
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2026-01-18 20:58:19 +0100

    Website polish and build scripts

---
+++ Build/Publish-PackageGitHub.ps1
@@ -0,0 +1,11 @@
+﻿$GitHubAccessToken = Get-Content -Raw 'C:\Support\Important\GithubAPI.txt'
+
+$publishGitHubReleaseAssetSplat = @{
+    ProjectPath          = "$PSScriptRoot\..\CodeGlyphX"
+    GitHubAccessToken    = $GitHubAccessToken
+    GitHubUsername       = "EvotecIT"
+    GitHubRepositoryName = "CodeGlyphX"
+    IsPreRelease         = $false
+}
+
+Publish-GitHubReleaseAsset @publishGitHubReleaseAssetSplat
